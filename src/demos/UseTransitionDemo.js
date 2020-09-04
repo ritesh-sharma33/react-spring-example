10c357e2e2885a1aeca8b29dd6b8cae251900c97
ritesh-sharma33 <sharmaritesh3312@gmail.com>
2020-09-04 22:37:15 +0530

    Animation axis changed

--- src/demos/UseTransitionDemo.js
+++ src/demos/UseTransitionDemo.js
@@ -39,10 +39,12 @@ export const UseTransitionDemoPage = () => {
     []
   );
   const transitions = useTransition(index, p => p, {
-    from: { opacity: 0, transform: "translate3d(100%,0,0)" },
-    enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
+    from: { opacity: 0, transform: "translate3d(0, 100%, 0)" },
+    enter: { opacity: 1, transform: "translate3d(0, 0%, 0)" },
     leave: { opacity: 0, transform: "translate3d(-50%,0,0)" }
   });
+
+  console.log(transitions);
 
   return (
     <div className="useTransitionDemoPage">
